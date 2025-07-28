18cf1b68db8a0254f093557d80d022fc207006a8
isak <mwangasuisack@gmail.com>
2025-07-28 12:29:07 +0300

    added FAQ

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 // Sample product data
 const cars = [
   { id: 1, name: "Toyota Hilux", category: "Truck", image: "assets/img/Hilux front.jpeg", description: "Reliable and fuel efficient." },
-  { id: 2, name: "Nissan X-Trail", category: "SUV", image: "images/xtrail.jpg", description: "Perfect for off-road adventures." },
-  { id: 3, name: "Isuzu D-Max", category: "Sedan", image: "images/dmax.jpg", description: "Built for hauling heavy loads." }
+  { id: 2, name: "Land Rover Discovery", category: "SUV", image: "assets/img/landrover.2.jpeg", description: "Perfect for off-road adventures." },
+  { id: 3, name: "Subaru", category: "Sedan", image: "assets/img/Impreza1.jpeg", description: "Built for hauling heavy loads." }
 ];
 
 // Load products
@@ -18,7 +18,7 @@ function loadProducts(data) {
           <div class="card-body">
             <h5 class="card-title product-title">${car.name}</h5>
             <p class="card-text">${car.description}</p>
-            <button class="btn btn-primary" onclick="showDetails(${car.id})">View Details</button>
+            <button class="btn btn-dark" onclick="showDetails(${car.id})">View Details</button>
           </div>
         </div>
       </div>
